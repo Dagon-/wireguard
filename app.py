0d13b4729a5d66581afd87cee60fe71607a1cbec
Dagon <dagoncal@gmx.com>
2020-09-29 19:53:18 +0100

    env variables for account and region

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python3
 
+import os
 from aws_cdk import core
 from wireguard.wireguard_stack import WireguardCdkStack
 
-env_eu = core.Environment(account="585823398980", region="eu-west-1")
+env_eu = core.Environment(
+    account=os.environ["CDK_DEFAULT_ACCOUNT"],
+    region=os.environ["CDK_DEFAULT_REGION"]
+)
 
 app = core.App()
 WireguardCdkStack(app, "wireguard", env=env_eu)
 
 core.Tag.add(app, "resource-group", "wireguard",
-  exclude_resource_types = ["AWS::ResourceGroups::Group"] # Tagging the resource group fails
+	exclude_resource_types = ["AWS::ResourceGroups::Group"] # Tagging the resource group fails
 )
 
 app.synth()
